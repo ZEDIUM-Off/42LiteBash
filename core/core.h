2fb9244aa11aeee0e257e8f6652adb3d5278e550
matisse chenavas <matisse.chenavas@gmail.com>
2023-05-31 14:26:52 +0200

    start point

--- core/core.h
+++ core/core.h
@@ -6,7 +6,31 @@
 /*   By:  mchenava < mchenava@student.42lyon.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:21:06 by  mchenava         #+#    #+#             */
-/*   Updated: 2023/05/31 14:21:07 by  mchenava        ###   ########.fr       */
+/*   Updated: 2023/05/31 14:26:50 by  mchenava        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CORE_H
+#define CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+// Structure for a command
+typedef struct s_command
+{
+    char **args; // Arguments for the command
+    int pipe;    // Indicates if the command should be piped
+} t_command;
+
+// Function prototypes
+void shell_loop(void);
+char *read_line(void);
+t_command **parse_line(char *line);
+int execute_commands(t_command **commands);
+
+#endif
