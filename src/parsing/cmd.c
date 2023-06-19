cbfb75a2d551c00f766c3d529f482895521542d3
Matisse <matisse.chenavas@gmail.com>
2023-06-19 18:15:26 +0200

    pulling

--- src/parsing/cmd.c
+++ src/parsing/cmd.c
@@ -18,27 +18,37 @@ t_cmd	*new_cmd(t_uint p_start, t_uint p_end)
 {
 	t_uint	n;
 	t_uint	o;
+	t_uint	a;
 	t_cmd	*cmd;
 
 	n = 0;
 	o = 0;
+	a = 0;
+	cmd = (t_cmd *)malloc(sizeof(t_cmd));
 	while (g_shx->line_split[p_start] && p_start != p_end)
 	{
 		printf("g_shx->line_split[p_start] = %s\n", g_shx->line_split[p_start]);
-		if (n = 0)
+		if (n == 0)
 		{
 			if (g_shx->line_split[p_start] != PIPE)
 				p_start++;
 			cmd->cmd = g_shx->line_split[p_start];
 			printf("cmd->cmd = %s\n", cmd->cmd);
+			n++;
 		}
-		if (g_shx->line_split[p_start][0] == "-")
+		else if (g_shx->line_split[p_start][0] == "-")
 		{
 			cmd->opt[o] = g_shx->line_split[p_start];
 			printf("cmd->opt[%li] = %s\n", o, cmd->opt[o]);
 			o++;
 		}
+		else
+		{
+			cmd->arg[a] = g_shx->line_split[p_start];
+			printf("cmd-arg[%li] = %s\n", a, cmd->arg[a]);
+			a++;
+		}
 		p_start++;
 	}
-	return (NULL);
+	return (cmd);
 }
